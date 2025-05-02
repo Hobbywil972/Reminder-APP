dbecb03e252905c89e8666d7c934c959bca4504f
Hobbywil972 <wg@infodom.com>
2025-05-02 19:18:43 -0400

    NextAuth est désormais configuré sans le préfixe __Secure-

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -19,7 +19,7 @@ export const authOptions = {
   cookies: {
     sessionToken: {
       name: process.env.NODE_ENV === "production"
-        ? "__Secure-next-auth.session-token"
+        ? "next-auth.session-token"
         : "next-auth.session-token",
       options: {
         httpOnly: true,
